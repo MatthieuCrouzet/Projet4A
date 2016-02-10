986361c957664e694faed79184e348a788b786d9
Matthieu Crouzet <MatthieuCrouzet@github.com>
2016-02-10 17:53:23 +0100

    voila enfin un depart

---
+++ test/app/scripts/directives/sidebar/sidebar-log/sidebar-log.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name izzyposWebApp.directive:adminPosHeader
+ * @description
+ * # adminPosHeader
+ */
+
+angular.module('sbAdminApp')
+  .directive('sidebarLog',function() {
+    return {
+      templateUrl:'scripts/directives/sidebar/sidebar-log/sidebar-log.html',
+      restrict: 'E',
+      replace: true,
+      scope: {
+      },
+      controller:function($scope){
+        $scope.selectedMenu = 'home';
+      }
+    }
+  });

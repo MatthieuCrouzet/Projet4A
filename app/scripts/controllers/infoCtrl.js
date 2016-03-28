0b1da2266fda5fdd7d7dee527a0c06b1930c4244
Matthieu Crouzet <MatthieuCrouzet@github.com>
2016-03-28 16:07:16 +0200

    ajout d'un graphique

--- app/scripts/controllers/infoCtrl.js
+++ app/scripts/controllers/infoCtrl.js
@@ -7,10 +7,40 @@
  * Controller of the sbAdminApp
  */
 angular.module('sbAdminApp')
- .controller('InfoCtrl', ['$scope', 'JobFact' , function ($scope, JobFact) {
+ .controller('InfoCtrl', ['$scope', 'JobFact', function ($scope, JobFact) {
   	$scope.user = "docker";
     $scope.jobs = JobFact.getJobs();
-      	$scope.stopJob = function(job){
+    $scope.pie = {
+      'data' : [],
+      'labels' : [
+        'In progress',
+        'Pending',
+        'Finish'
+      ]
+    };
+    $scope.getStates = function(){
+      var inprogress = 0; 
+      var pending = 0; 
+      var finish = 0;
+      var states = [];
+      angular.forEach($scope.jobs,function(value, key){
+        if(value['State']=='In progress'){
+          inprogress++;
+        }else if(value['State']=='Pending'){
+          pending++;
+        }else if(value['State']=='Finish'){
+          finish++;
+        }
+      })
+      states.push(inprogress);
+      states.push(pending);
+      states.push(finish);
+      return states;
+    }
+    $scope.$watchCollection('getStates',function(){
+      $scope.pie['data'] = $scope.getStates();
+    });
+    $scope.stopJob = function(job){
       var r = confirm("Are you sure you want to stop this job?")
       if(r==true){
         alert('The job have been stopped');
